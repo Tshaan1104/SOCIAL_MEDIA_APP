a90f9b0e10d1a3fa44054d535f8868307462fafb
Tshaan1104 <tshaan1104@gmail.com>
2024-08-17 00:03:41 +0530

    authentication

--- Backend/controllers/Auth.js
+++ Backend/controllers/Auth.js
@@ -1,15 +1,59 @@
-const register = (req, res) => {
-    // Handle user registration logic here
-    res.json({ message: 'User registered successfully!' });
-  };
-  
-  const login = (req, res) => {
-    // Handle user login logic here
-    res.json({ message: 'User logged in successfully!' });
-  };
-  
-  module.exports = {
-    register,
-    login,
-  };
-  
+import bcrypt from 'bcrypt';
+import jwt from 'jwt';
+import User from '../models/User.js'
+
+const generateToken=(id)=>{
+  const jwtSecret='';
+  return jwt.sign({id},jwtSecret,{
+    expiresIn:'30d',
+  });
+}
+
+export const register= async(req, res) =>{
+  try{
+    const {username,password,email,profileimg} = req.body;
+    const salt= await bcrypt.genSalt();
+    const passwordHash= await bcrypt.hash(password,salt);
+
+    const newUser=new User({
+      username,
+      password:passwordHash,
+      email,
+      profileimg
+    });
+
+
+    const user = await newUser.save();
+    const token=generateToken(user._id);
+
+    const userdata={_id:user._id,username:user.username,email:user.email,profileimg:user.profileimg,about:user.about,posts:user.posts,followers:user.followers,following:user.following};
+
+    res.status(200).json({token,user:userdata});
+  }
+  catch (err) {
+    res.status(500).json({error:err.message});
+  }
+
+};
+
+export const login = async(req, res) => {
+  try{
+    const {email,password} = req.body;
+    const user =await Users.findOne({email:email});
+    if (!user) return res.status(400).json({msg: 'User not found'});
+
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) return res.status(400).json({msg: 'Invalid Credentials'});
+
+    const token=generateToken(user._id);
+    delete user.password;
+    const userdata= {_id:user._id, username:user.username, email:user.email, profileimg:user.profileimg, about:user.about, posts:user.posts, followers:user.followers, following:user.following};
+    res.status(200).json({token,user:userdata});
+
+    console.log(token,userdata);
+
+  }
+  catch (err) {
+    res.status(500).json({error:err.message});
+  }
+}
