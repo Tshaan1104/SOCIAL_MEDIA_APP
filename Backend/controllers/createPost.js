7cd5b8603120f958a3bdbb1d2e6135fa664abb34
Tshaan1104 <tshaan1104@gmail.com>
2024-08-23 16:13:32 +0530

    create post

---
+++ Backend/controllers/createPost.js
@@ -0,0 +1,10 @@
+import Post from "../models/Post.js";
+
+export const createpost=async (req, res) => {
+    try {
+        const newPost=new Post(req.body);
+        const post =await newPost.save();
+    }
+    catch (err) {
+        res.status(500).json({error:err});
+}}
