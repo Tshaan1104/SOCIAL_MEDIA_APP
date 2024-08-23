f56cf3ecadea3fc0766870fdc8d11f16112968ff
Tshaan1104 <tshaan1104@gmail.com>
2024-08-23 16:15:31 +0530

    Posts

---
+++ Backend/controllers/Posts.js
@@ -0,0 +1,51 @@
+import User from "../models/Users.js";
+import Stories from "../models/Stories.js";
+import Posts from "../models/Post.js";
+
+
+export const fetchAllPosts=async (req, res, next) =>{
+ try {
+    const posts=await Posts.find().sort({_id:-1});
+    res.json(posts);
+ }
+ catch (err) {
+    console.error(err);
+    res.status(500).json({error:'Server error'});
+ }
+}
+
+
+export const fetchUserName=async (req, res) =>{
+        try{
+            const userId=req.body.userId;
+            const user = await User.findById(userId);
+            console.log(userId);
+            res.status(200).json(user);
+        }
+        catch(error){
+            console.log(error);
+            res.status(500).json({error: error.message});
+        }
+}
+
+export const fetchUserImg=async(req,res)=>{
+    try {
+        const userId=req.body.userId;
+        const user= await User.findOne({_id:userId});
+        console.log(userId);
+        res.status(200).json(user);
+    }catch(error){
+        console.error(error);
+        res.status(500).json({error:'Server error'});
+    }
+}
+
+export const fetchAllStories =async(req,res)=>{
+    try{
+        const stories = await Stories.find();
+        res.status(200).json(stories);
+    }catch(error){
+        console.log(error);
+        res.status(500).json({error:'Server error'});
+    }
+}
